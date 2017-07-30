b17ce8b054bf44423d4c2e9008fa9f5d05cba711
Naz <nazmalik08@gmail.com>
2017-07-30 13:20:50 +0100

    Test get_best_move win and loss prevention scenarios

--- lib/computer.rb
+++ lib/computer.rb
@@ -10,40 +10,59 @@ class Computer
   end
 
   def choose_square(board)
-    board[4] == MIDDLE_SQUARE ? 4 : get_best_move(board, @marker)
+    board[4] == MIDDLE_SQUARE ? 4 : get_best_move(board)
   end
 
-  def get_best_move(board, next_player, depth = 0, best_score = {})
+  def get_best_move(board)
     empty_squares = []
     best_move = nil
+
+    #get array of available squares
     board.each do |square|
-      if square != COMPUTER_MARKER && square != HUMAN_MARKER
+      unless square == "X" || square == "O"
         empty_squares << square
       end
     end
-    empty_squares.each do |squares|
-      board[squares.to_i] = @computer
+
+    empty_squares.each do |square|
+      #if choosing the square results in a win, choose it
+      board[square.to_i] = @marker
       if game_is_over(board)
-        best_move = squares.to_i
-        board[squares.to_i] = squares
+        best_move = square.to_i
+        board[square.to_i] = square
         return best_move
       else
-        board[squares.to_i] = @human
+        #if the other player choosing the square wins, choose it
+        board[square.to_i] = "O"
         if game_is_over(board)
-          best_move = squares.to_i
-          board[squares.to_i] = squares
+          best_move = square.to_i
+          board[square.to_i] = square
           return best_move
         else
-          board[squares.to_i] = squares
+          #reset the square
+          board[square.to_i] = square
         end
       end
     end
+
+    #if no best_move found, make a random move
     if best_move
       return best_move
     else
       n = rand(0..empty_squares.count)
       return empty_squares[n].to_i
     end
+  end
+
+  def game_is_over(board)
+    [board[0], board[1], board[2]].uniq.length == 1 ||
+    [board[3], board[4], board[5]].uniq.length == 1 ||
+    [board[6], board[7], board[8]].uniq.length == 1 ||
+    [board[0], board[3], board[6]].uniq.length == 1 ||
+    [board[1], board[4], board[7]].uniq.length == 1 ||
+    [board[2], board[5], board[8]].uniq.length == 1 ||
+    [board[0], board[4], board[8]].uniq.length == 1 ||
+    [board[2], board[4], board[6]].uniq.length == 1
   end
 
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -10,7 +10,7 @@ describe Computer do
     end
   end
 
-  describe '#get_computer_square' do
+  describe '#choose_square' do
 
     it 'chooses the middle square when it is available' do
       board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
@@ -22,6 +22,26 @@ describe Computer do
       expect(computer).to receive(:get_best_move).and_return 1
       computer.choose_square(board)
     end
+
+  end
+
+  describe '#get_best_move' do
+
+    it 'will seize the win' do
+      board = ["0", "X", "2", "O", "X", "O", "6", "7", "8"]
+      expect(computer.get_best_move(board)).to eq 7
+    end
+
+    it 'will prevent a loss' do
+      board = ["X", "1", "2", "O", "O", "5", "6", "7", "X"]
+      expect(computer.get_best_move(board)).to eq 5
+    end
+
+    it 'will seize the win, even when within one move of losing' do
+      board = ["X", "X", "2", "3", "O", "O", "6", "7", "8"]
+      expect(computer.get_best_move(board)).to eq 2
+    end
+
 
 
   end
