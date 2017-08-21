6c250a24767028bc89d6b972bb4bc0c5fff82fc2
Naz <nazmalik08@gmail.com>
2017-08-21 16:56:14 +0100

    Test and move user input to Human class

--- lib/human.rb
+++ lib/human.rb
@@ -8,4 +8,18 @@ class Human
     @marker = MARKER
   end
 
+  # gets input (Human)
+  # checks if square is free (ask Board.occupied?(square))
+  # if not, gets input (Human)
+  # places marker (Board.place_marker)
+
+  def move
+    get_move
+  end
+
+  def get_move
+    gets.chomp.to_i
+  end
+
+
 end
