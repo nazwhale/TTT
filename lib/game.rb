b41e1cb6b97dc961806996a7639c150c620caf17
Naz <nazmalik08@gmail.com>
2017-08-21 16:24:35 +0100

    Extract make_human_move and make_computer_move from play method

--- lib/game.rb
+++ lib/game.rb
@@ -17,28 +17,31 @@ class Game
     show_current_board
     # loop through until the game is won or tied
     until @board.game_over?
-
-      humans_choice = nil
-      #if choice taken, ask again
-      until humans_choice
-        humans_choice = gets.chomp.to_i
-        current_value = @board.state[humans_choice]
-        humans_choice = nil if @board.occupied?(current_value)
-      end
-
-      @board.state[humans_choice] = @human.marker
-
-      unless @board.game_over?
-        computers_choice = @computer.get_square(@board)
-        @board.state[computers_choice] = @computer.marker
-      end
-
+      make_human_move
+      make_computer_move
       show_current_board
     end
     Messages.game_over_message
   end
 
   private
+
+  def make_human_move
+      humans_choice = nil
+      until humans_choice
+        humans_choice = gets.chomp.to_i
+        current_value = @board.state[humans_choice]
+        humans_choice = nil if @board.occupied?(current_value)
+      end
+      @board.state[humans_choice] = @human.marker
+  end
+
+  def make_computer_move
+    unless @board.game_over?
+      computers_choice = @computer.get_square(@board)
+      @board.state[computers_choice] = @computer.marker
+    end
+  end
 
   def show_current_board
     Messages.print_board(board)
