52011e8997ba54d003a43d839bf14dffe57717b2
Naz <nazmalik08@gmail.com>
2017-08-31 13:14:16 +0100

    Write test to set human symbol on instantiation

--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -2,11 +2,11 @@ require 'human'
 
 describe Human do
 
-  subject(:human) { described_class.new }
+  subject(:human) { described_class.new("O") }
 
   describe '#initialize' do
     it 'has a symbol' do
-      expect(human.symbol).to eq Human::SYMBOL
+      expect(human.symbol).to eq "O"
     end
   end
 
