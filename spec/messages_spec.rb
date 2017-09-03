124f0acc435273e4ea866f0b11b0f6ca23bfc24a
Naz <nazmalik08@gmail.com>
2017-09-03 15:53:31 +0100

    Add win messages

--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -20,10 +20,45 @@ describe Messages do
     end
   end
 
+  describe '#choose_player1_symbol' do
+    it 'displays appropriate message' do
+      message = "Choose a symbol for player 1!\n"
+      expect{ Messages.choose_player1_symbol }.to output(message).to_stdout
+    end
+  end
+
+  describe '#choose_player2_symbol' do
+    it 'displays appropriate message' do
+      message = "What symbol will player 2 use?\n"
+      expect{ Messages.choose_player2_symbol }.to output(message).to_stdout
+    end
+  end
+
+  describe '#promt_game_type' do
+    it 'displays appropriate message' do
+      message = "What kind of game would you like to play?\nSelect 1 for 😌  vs. 😌 \nSelect 2 for 😌  vs. 🤖 \nSelect 3 for 🤖  vs. 🤖 \nYour choice: "
+      expect{ Messages.prompt_game_type }.to output(message).to_stdout
+    end
+  end
+
   describe '#game_type_confirmation' do
     it 'outputs the type of game' do
-      confirmation = "You chose to play Human vs. Computer\n"
+      confirmation = "\nYou chose to play Human vs. Computer\n\n"
       expect{ Messages.game_type_confirmation("Human", "Computer") }.to output(confirmation).to_stdout
+    end
+  end
+
+  describe '#try_again' do
+    it 'displays appropriate message' do
+      message = "Invalid input! Please try again...\n\n"
+      expect{ Messages.try_again }.to output(message).to_stdout
+    end
+  end
+
+  describe '#prompt_move' do
+    it 'displays appropriate message' do
+      message = "Choose your move! Enter a number between 0 and 8...\n"
+      expect{ Messages.prompt_move }.to output(message).to_stdout
     end
   end
 end
