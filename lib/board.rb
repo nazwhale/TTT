648db3c42da4c7389073ec79f75b4b3de03ab5a4
Naz Malik <naz@Nazs-MacBook-Pro.local>
2017-09-27 18:19:29 +0100

    Compute rows and diagonals in Board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,7 @@
 class Board
 
   EMPTY_BOARD = Array.new(9)
+  EMPTY_SQUARE = nil
   CORNERS = [0, 2, 6, 8]
   attr_reader :state
   attr_writer :state
@@ -10,11 +11,11 @@ class Board
   end
 
   def occupied?(index)
-    @state[index.to_i] != nil
+    @state[index.to_i] != EMPTY_SQUARE
   end
 
   def empty?
-    @state.all? { |square| square == nil } 
+    @state.all? { |square| square == EMPTY_SQUARE } 
   end
 
   def game_over?(player1, player2)
@@ -22,11 +23,11 @@ class Board
   end
 
   def tie?(player1, player2)
-    anyone_won?(player1, player2) ? false : board_full?
+    board_full? && !anyone_won?(player1, player2)
   end
 
   def win?(player)
-    win_scenarios.any? { |line| line.count(player.symbol) == 3 }
+    win_scenarios.any? { |line| line.count(player.symbol) == root_board_size }
   end
 
   def get_corners
@@ -35,27 +36,32 @@ class Board
 
   private
 
+  def board_size
+    @state.length
+  end
+
+  def root_board_size
+    Math.sqrt(board_size)
+  end
+
   def win_scenarios
-    rows + columns + diagonals
+    rows + columns + left_diagonal + right_diagonal
   end
 
   def rows
-    [
-      [@state[0], @state[1], @state[2]],
-      [@state[3], @state[4], @state[5]],
-      [@state[6], @state[7], @state[8]]
-    ]
+    @state.each_slice(root_board_size).to_a
   end
 
   def columns
     rows.transpose
   end
 
-  def diagonals
-    [
-      [@state[0], @state[4], @state[8]],
-      [@state[2], @state[4], @state[6]]
-    ]
+  def left_diagonal
+    [rows.collect.with_index { |row, index| row[index] }]
+  end
+
+  def right_diagonal
+    [rows.collect.with_index { |row, index| row.reverse[index] }]
   end
 
   def anyone_won?(player1, player2)
@@ -63,6 +69,6 @@ class Board
   end
 
   def board_full?
-    @state.none? { |square| square == nil } 
+    @state.none? { |square| square == EMPTY_SQUARE } 
   end
 end
